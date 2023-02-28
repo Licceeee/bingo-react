89c3585e57c04944e025b743a06dc2210876bedd
Alicia Schonefeld <schonefeld.dev@gmail.com>
2023-02-28 19:47:31 +0100

    add shuffleData func

---
+++ src/utils/shuffleData.jsx
@@ -0,0 +1,14 @@
+const shuffle = (arr) => {
+	let current = arr.length; // 25
+	let random; // 0
+
+	while (current != 0) {
+		random = Math.floor(Math.random() * current);
+		current--; // decrease current
+		// swap values using destructuring
+		[arr[current], arr[random]] = [arr[random], arr[current]];
+	}
+	return arr;
+};
+
+export default shuffle;
