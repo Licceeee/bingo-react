ec7269de2ad6764ca09af0978c11465e9bf68918
Alicia Schonefeld <schonefeld.dev@gmail.com>
2023-02-28 19:46:42 +0100

    upd styling

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { ThemeProvider, CssBaseline } from "@mui/material";
 
 import App from "./App";
 import "./index.css";
-import theme from "./theme";
+import theme from "./theme/theme";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
