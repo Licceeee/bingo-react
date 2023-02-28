6d248b5f5fa1beb0afac79df95c88ddccb4f30d3
Alicia Schonefeld <schonefeld.dev@gmail.com>
2023-02-28 19:43:34 +0100

    upd theme

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -2,8 +2,16 @@ import { createTheme, responsiveFontSizes } from "@mui/material/styles";
 
 const theme = createTheme({
 	palette: {
+		background: {
+			default: "#F3EAD8",
+		},
 		primary: {
+			main: "#E0C3B1",
+		},
+		secondary: {
+			dark: "#c3c1ac",
 			main: "#C3C1AC",
+			light: "#e7e6dd",
 		},
 	},
 });
