18882a658b0b7da8624e0d7ea64bfcde37d00465
Alicia Schonefeld <schonefeld.dev@gmail.com>
2023-02-28 19:46:13 +0100

    add navbar

---
+++ src/components/Nav.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { AppBar, Toolbar, Button, Typography } from "@mui/material";
+
+export default function Nav() {
+	return (
+		<AppBar position="sticky" color="primary">
+			<Toolbar sx={{ display: "flex", justifyContent: "space-around" }}>
+				<Typography>Bingo React Challange</Typography>{" "}
+				<Button variant="contained" color="secondary">
+					Restart (TODO)
+				</Button>
+			</Toolbar>
+		</AppBar>
+	);
+}
